be5330ffb70ba38f4a51d50ca5272a10ba757615
bug5654 <bug.new@gmail.com>
2015-12-27 14:59:34 -0800

    added project files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # gcdproj
 Peer analysis for getting and cleaning data
+
+
+Reasonably straightforward analysis.  up until the begin merging comment, the script is just reading csv files into memory.
+
+Once the all-too-many files are in memory, we use the next 3 lines to merge the test and training data.
+
+After this, we apply the names from the features file to the huge dataset in xdat, then scrape that for only columns with the word, "mean" in them, dropping the rest of the columns to create the first tidyset variable.  This leaves us with a large table without unique values.  the dplyr library is then loaded to do the somewhat complicated line that averages all the columns once grouped by activity and subject, that is that we take BOTH columns to be a combined key, then apply the mean() function down each column in order to leave us with 1 reading per subject/activity pair.  Finally, we write out the table with row.names=FALSE as per instructions.
---
+++ supermerge.R
@@ -0,0 +1,31 @@
+#read in test and training duplicate data
+#start in root folder
+labels <- read.table("./UCI HAR Dataset/activity_labels.txt", col.names = 
+                        c("activityInd", "activity"))
+names <- read.table("./UCI HAR Dataset/features.txt")
+
+#trainfolder
+trainsub <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+trainact <- read.table("./UCI HAR Dataset/train/y_train.txt", col.names = "activity")
+trainx <- read.table("./UCI HAR Dataset/train/X_train.txt")
+trainy <- read.table("./UCI HAR Dataset/train/Y_train.txt", col.names="activityy")
+
+#testfolder
+testsubj <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+testact <- read.table("./UCI HAR Dataset/test/y_test.txt", col.names = "activity")
+testx <- read.table("./UCI HAR Dataset/test/X_test.txt")
+testy <- read.table("./UCI HAR Dataset/test/Y_test.txt", col.names="activityy")
+
+#begin merging
+subject<- rbind(testsubj, trainsub)
+act <- rbind(testact, trainact)
+xdat <- rbind(testx, trainx)
+names(xdat) <- names[,2]
+meansx <- xdat[,grep("mean",names(xdat))]
+tidyset <- cbind(subject, act, meansx)
+#I know I've done this without dplyr before...but can't find how, oh well, thanks google
+tidyset4<-tidyset3 %<>% group_by(activity, subject) %>% summarise_each(funs(mean))6
+
+#ydat <- rbind(testy, trainy)
+#names(ydat) <- names[,2]
+#6meansy <- ydat[grepl("*mean*",names(ydat)) | grepl("*std*",names(ydat)),]
